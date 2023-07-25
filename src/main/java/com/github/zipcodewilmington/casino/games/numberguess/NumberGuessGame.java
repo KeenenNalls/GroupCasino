b335d8d3a04797b4ff4bff8edbe975e8b16ffe7c
Keenen <keenen@Zipcoders-MacBook-Pro.local>
2023-07-25 08:33:21 -0400

    no changes

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -5,13 +5,14 @@ package com.github.zipcodewilmington.casino.games.numberguess;
  */
 import java.util.Scanner;
 public class NumberGuessGame {
-        private Player player;
-        private Scanner scanner;
-        private int targetNumber;
-        private int numberOfTries;
-        private String playAgainChoice;
+        private Player player; //This line declares a private variable named player of type Player. In this context, Player refers to a class that represents a player in the number guess game. By declaring it as private, the variable is only accessible within the class it is declared in (in this case, the NumberGuessGame class).
+        private Scanner scanner; //This line declares a private variable named scanner of type Scanner. A Scanner is a Java class that allows reading user input from the console. Declaring it as private means that it is only accessible within the NumberGuessGame class.
+        private int targetNumber; //This line declares a private variable named targetNumber of type int. It is used to store the randomly generated number that the player needs to guess in the number guess game.
+        private int numberOfTries; //This line declares a private variable named numberOfTries of type int. It is used to keep track of the number of remaining attempts the player has to guess the correct number. The game may end after a certain number of tries.
+        private String playAgainChoice; //This line declares a private variable named playAgainChoice of type String. It is used to store the player's choice of whether to play the game again or not. The game may prompt the player to input either "yup" or "nah" (yes or no) to determine if they want to play another round.
 
-        public NumberGuessGame() {
+    //By declaring these variables as private, the NumberGuessGame class encapsulates its internal state, ensuring that other classes cannot directly access or modify these variables. Instead, other classes interact with the NumberGuessGame class through its public methods, which control and manage the game's behavior based on the values stored in these private variables.
+    public NumberGuessGame() {
             this.scanner = new Scanner(System.in);
             this.numberOfTries = 3;
         }
@@ -21,20 +22,20 @@ public class NumberGuessGame {
         }
 
         public void run() {
-            System.out.println(displayInstructions());
-            boolean playAgain = true;
-            while (playAgain) {
+            System.out.println(displayInstructions()); //This line prints the game's instructions to the console. It tells the player to guess a number between 1 and 10.
+            boolean playAgain = true;  //This line sets a boolean variable
+            while (playAgain) { //This line starts a loop that will keep running the game as long as playAgain is true.
                 targetNumber = (int) (Math.random() * 10) + 1; // Generate a random number between 1 and 10
                 numberOfTries = 3;
-                boolean isWinner = false;
+                boolean isWinner = false; //It will later be used to check if the player guessed the correct number.
 
-                while (!isWinner && numberOfTries > 0) {
-                    int userGuess = getUserGuess();
-                    isWinner = checkWinner(userGuess);
-                    numberOfTries--;
+                while (!isWinner && numberOfTries > 0) { //This line starts another loop that runs as long as the player hasn't won
+                    int userGuess = getUserGuess(); //This line gets the player's guess from the user (input from the console) and stores it in the userGuess variable.
+                    isWinner = checkWinner(userGuess); //This line checks if the userGuess is equal to the targetNumber (the correct number to guess). If the guess is correct, isWinner becomes true, and the player wins.
+                    numberOfTries--; //This line decreases the number of remaining tries by 1, as the player made an attempt.
                     if (isWinner) {
                         System.out.println("Congratulations!!!! You LUCKILY guessed the right number: " + targetNumber);
-                    } else if (numberOfTries > 0) {
+                    } else if (numberOfTries > 0) { //the player's guess is incorrect, but they have more attempts left. The game prints a message encouraging the player to try again.
                         System.out.println("Better Luck Next Time!");
                     }
                     else {
---
+++ src/test/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessTest.java
@@ -0,0 +1,14 @@
+package com.github.zipcodewilmington.casino.games.numberguess;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NumberGuessTest {
+
+    @Test
+    public void checkWinnerTest() {
+        int userGuess = 5;
+
+    }
+}
+
+
